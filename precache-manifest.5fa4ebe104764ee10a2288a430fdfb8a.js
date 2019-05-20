14b62b870600601ed7dd966455e507801d37cd6a
Raphael Sefakor Adinkrah <sefakorhom2012@gmail.com>
2019-05-19 22:26:48 -0700

    Updates

---
+++ precache-manifest.5fa4ebe104764ee10a2288a430fdfb8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8444430100fd388f76d31a6919f94f6e",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "47ff26ebb73877fb1517",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "67ffbbc966933d814afb",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "47ff26ebb73877fb1517",
+    "url": "/contactmanager/static/js/2.03fd6fea.chunk.js"
+  },
+  {
+    "revision": "67ffbbc966933d814afb",
+    "url": "/contactmanager/static/js/main.5a84d552.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
